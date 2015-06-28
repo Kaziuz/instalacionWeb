a7d64cd7ba31ef37f72997f748f573346311ddd1
kaziuz <johnnyalexandersepulveda@gmail.com>
2015-06-28 18:46:16 -0500

    añado script para dibujar, fañta conectar esto con el index de la pagina 2

--- otraPagina/js/sketch.js
+++ otraPagina/js/sketch.js
@@ -1,28 +1,8 @@
-
-
-function setup() {
-   // El canvas ocupa toda la pantalla
-  var plano1 = createCanvas(640, 480);
-  plano1.parent("contenedor1");
-}
-
-function draw() {
-   
-   // primera imagen
-
-   strokeWeight(4);
-   stroke(255, 0, 0);
-   line(150, 205, 500, 500);
-   point(150, 205);
-
-   // añadimos un texto
-   noStroke();
-   textSize(15);
-   textAlign(CENTER);
-   textFont("Helvetica");
-   text("Aqui vamos a escribir algun texto que tengamos", 500, 520);
-}
-
-
-
+var canvas = document.getElementById("plano1");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.moveTo(20, 20);
+ctx.lineTo(20, 100);
+ctx.lineTo(70, 100);
+ctx.stroke();
 
