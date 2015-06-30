7551236c4b87d4fc0de781340bb1bcaf05bd566e
kaziuz <johnnyalexandersepulveda@gmail.com>
2015-06-30 02:54:05 -0500

    añado cambios al nombres del script de la pagina de arriba

---
+++ otraPagina/js/lecturaImgCabecera.js
@@ -0,0 +1,67 @@
+window.onload = function()
+{
+    var img0  = $("#img1, col-md-2");
+	var img1  = $("#img2, col-md-2");
+	var img2  = $("#img3, col-md-2");
+	var img3  = $("#img4, col-md-2");
+	var img4  = $("#img5, col-md-2");
+
+	var cont = 0;
+	var log = document.getElementById('log');
+	var	fotos = [];		
+
+	setInterval(function()
+	{
+		
+		cont += 1;
+		console.log("contador:"+cont);
+
+		// se pregunta si hay mas fotos nuevas en el DOM y actualiza el contenido
+		if(cont == 1)
+		{
+			$.ajax({
+				url:"http://localhost:8080",
+				success: function(data){
+					fotos = [];
+					$(data).find("td > a").each(function(){
+						fotos.push( $(this).attr("href"));
+					});
+				} 
+
+			});
+
+			// esto me imprime las fotos
+		}
+		else if(cont == 5)
+		{
+			img0.attr('src', 'http://localhost:8080/'+fotos[1]);
+			img1.attr('src', 'http://localhost:8080/'+fotos[2]);
+			img2.attr('src', 'http://localhost:8080/'+fotos[3]);
+			img3.attr('src', 'http://localhost:8080/'+fotos[4]);
+			img4.attr('src', 'http://localhost:8080/'+fotos[5]);
+		}
+		else if(cont == 7)
+		{
+			img0.attr('src', 'http://localhost:8080/'+fotos[6]);
+			img1.attr('src', 'http://localhost:8080/'+fotos[7]);
+			img2.attr('src', 'http://localhost:8080/'+fotos[8]);
+			img3.attr('src', 'http://localhost:8080/'+fotos[9]);
+			img4.attr('src', 'http://localhost:8080/'+fotos[10]);
+		}
+		else if(cont == 10)
+		{
+			img0.attr('src', 'http://localhost:8080/'+fotos[11]);
+			img1.attr('src', 'http://localhost:8080/'+fotos[12]);
+			img2.attr('src', 'http://localhost:8080/'+fotos[13]);
+			img3.attr('src', 'http://localhost:8080/'+fotos[14]);
+			img4.attr('src', 'http://localhost:8080/'+fotos[15]);
+		}
+		else if(cont == 12)
+		{
+			cont = 0;
+			console.log("restart");
+		};
+		log.value = cont;
+	},1000);
+}
+
