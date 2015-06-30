cf0bfc2527c06d70bf885f0123cc49e61444308b
kaziuz <johnnyalexandersepulveda@gmail.com>
2015-06-30 02:52:10 -0500

    añado actualizacion de los ultimos 5 objetos almacenados en el array

--- otraPagina/js/lecturaImg.js
+++ otraPagina/js/lecturaImg.js
@@ -14,7 +14,6 @@ window.onload = function()
 	{
 		
 		cont += 1;
-		console.log("contador:"+cont);
 		// se pregunta si hay mas fotos nuevas en el DOM y actualiza el contenido
 		if(cont == 1)
 		{
@@ -26,41 +25,18 @@ window.onload = function()
 						fotos.push( $(this).attr("href"));
 					});
 				} 
-			});
 
-			// esto me imprime las fotos
-			console.log(fotos);
-			// vamos a ver cuantas fotos tenemos
-			console.log("1 change");
+			});
 		}
-		else if(cont == 5)
+		else if(cont == 2)
 		{
-			img0.attr('src', 'http://localhost:8080/'+fotos[0]);
-			img1.attr('src', 'http://localhost:8080/'+fotos[1]);
-			img2.attr('src', 'http://localhost:8080/'+fotos[2]);
-			img3.attr('src', 'http://localhost:8080/'+fotos[3]);
-			img4.attr('src', 'http://localhost:8080/'+fotos[4]);
-			console.log("2 change");
-		}
-		else if(cont == 7)
-		{
-			img0.attr('src', 'http://localhost:8080/'+fotos[5]);
-			img1.attr('src', 'http://localhost:8080/'+fotos[6]);
-			img2.attr('src', 'http://localhost:8080/'+fotos[7]);
-			img3.attr('src', 'http://localhost:8080/'+fotos[8]);
-			img4.attr('src', 'http://localhost:8080/'+fotos[9]);
-			console.log("3 change");
+			img0.attr('src', 'http://localhost:8080/'+fotos[11]);
+			img1.attr('src', 'http://localhost:8080/'+fotos[12]);
+			img2.attr('src', 'http://localhost:8080/'+fotos[13]);
+			img3.attr('src', 'http://localhost:8080/'+fotos[14]);
+			img4.attr('src', 'http://localhost:8080/'+fotos[15]);
 		}
 		else if(cont == 10)
-		{
-			img0.attr('src', 'http://localhost:8080/'+fotos[10]);
-			img1.attr('src', 'http://localhost:8080/'+fotos[11]);
-			img2.attr('src', 'http://localhost:8080/'+fotos[12]);
-			img3.attr('src', 'http://localhost:8080/'+fotos[13]);
-			img4.attr('src', 'http://localhost:8080/'+fotos[14]);
-			console.log("4 change");	
-		}
-		else if(cont == 12)
 		{
 			cont = 0;
 			console.log("restart");
