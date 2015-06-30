f9a1c0fe27a113e4c8111fc6483d4ca64d1fa08f
kaziuz <johnnyalexandersepulveda@gmail.com>
2015-06-30 02:07:28 -0500

    modifico este script para que haga lecturas de una carpeta d imagenes almacendas en local

--- otraPagina/js/lecturaImg.js
+++ otraPagina/js/lecturaImg.js
@@ -8,18 +8,18 @@ window.onload = function()
 
 	var cont = 0;
 	var log = document.getElementById('log');
-	var	 fotos = [];		
+	var	fotos = [];		
 
 	setInterval(function()
 	{
 		
 		cont += 1;
-
+		console.log("contador:"+cont);
 		// se pregunta si hay mas fotos nuevas en el DOM y actualiza el contenido
 		if(cont == 1)
 		{
 			$.ajax({
-				url:"http://192.168.10.253:8080/",
+				url:"http://localhost:8080",
 				success: function(data){
 					fotos = [];
 					$(data).find("td > a").each(function(){
@@ -29,38 +29,38 @@ window.onload = function()
 			});
 
 			// esto me imprime las fotos
-			console.log("el numero de fotos:"+fotos);
+			console.log(fotos);
 			// vamos a ver cuantas fotos tenemos
 			console.log("1 change");
 		}
-		else if(cont == 2)
+		else if(cont == 5)
 		{
-			img0.attr('src', 'http://192.168.10.253:8080/'+fotos[0]);
-			img1.attr('src', 'http://192.168.10.253:8080/'+fotos[1]);
-			img2.attr('src', 'http://192.168.10.253:8080/'+fotos[2]);
-			img3.attr('src', 'http://192.168.10.253:8080/'+fotos[3]);
-			img4.attr('src', 'http://192.168.10.253:8080/'+fotos[4]);
+			img0.attr('src', 'http://localhost:8080/'+fotos[0]);
+			img1.attr('src', 'http://localhost:8080/'+fotos[1]);
+			img2.attr('src', 'http://localhost:8080/'+fotos[2]);
+			img3.attr('src', 'http://localhost:8080/'+fotos[3]);
+			img4.attr('src', 'http://localhost:8080/'+fotos[4]);
 			console.log("2 change");
 		}
-		else if(cont == 100)
+		else if(cont == 7)
 		{
-			img0.attr('src', 'http://192.168.10.253:8080/'+fotos[5]);
-			img1.attr('src', 'http://192.168.10.253:8080/'+fotos[6]);
-			img2.attr('src', 'http://192.168.10.253:8080/'+fotos[7]);
-			img3.attr('src', 'http://192.168.10.253:8080/'+fotos[8]);
-			img4.attr('src', 'http://192.168.10.253:8080/'+fotos[9]);
+			img0.attr('src', 'http://localhost:8080/'+fotos[5]);
+			img1.attr('src', 'http://localhost:8080/'+fotos[6]);
+			img2.attr('src', 'http://localhost:8080/'+fotos[7]);
+			img3.attr('src', 'http://localhost:8080/'+fotos[8]);
+			img4.attr('src', 'http://localhost:8080/'+fotos[9]);
 			console.log("3 change");
 		}
-		else if(cont == 150)
+		else if(cont == 10)
 		{
-			img0.attr('src', 'http://192.168.10.253:8080/'+fotos[10]);
-			img1.attr('src', 'http://192.168.10.253:8080/'+fotos[11]);
-			img2.attr('src', 'http://192.168.10.253:8080/'+fotos[12]);
-			img3.attr('src', 'http://192.168.10.253:8080/'+fotos[13]);
-			img4.attr('src', 'http://192.168.10.253:8080/'+fotos[14]);
+			img0.attr('src', 'http://localhost:8080/'+fotos[10]);
+			img1.attr('src', 'http://localhost:8080/'+fotos[11]);
+			img2.attr('src', 'http://localhost:8080/'+fotos[12]);
+			img3.attr('src', 'http://localhost:8080/'+fotos[13]);
+			img4.attr('src', 'http://localhost:8080/'+fotos[14]);
 			console.log("4 change");	
 		}
-		else if(contador == 200)
+		else if(cont == 12)
 		{
 			cont = 0;
 			console.log("restart");
